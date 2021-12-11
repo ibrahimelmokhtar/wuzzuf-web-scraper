3be288096be3bc84604d04dd50d1db0e6708ca4a
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2021-12-11 16:45:31 +0200

    get desired page content using the obtained URL

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from check_userInput import *
 from searchQuery import *
+import requests
 
 # get disired hob title from the user:
 desired_job = input("Enter job title: ")
@@ -8,5 +9,9 @@ check_jobTitle(desired_job)
 # set a search query on "wuzzuf.net":
 job_url = set_searchQuery(desired_job)
 print(job_url)
+
+# fetch the obrained URL, then get page content:
+page_content = requests.get(job_url).content
+print(page_content)
 
 print("Done!")
