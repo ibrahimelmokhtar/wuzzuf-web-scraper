b28366678ca5c58552e5084aed771d0e209a6b7a
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2021-12-14 23:12:02 +0200

    Create separate directory to save the output file

--- saveIntoFile.py
+++ saveIntoFile.py
@@ -1,13 +1,38 @@
 from datetime import datetime
 import csv
+import os
 
-def format_file_name(desired_job):
+def create_directory(file_type):
+    """create directory to save created files into it depending on file types (.csv, .txt, ... ect.)
+
+    Args:
+        file_type (String): desired file type (for example: csv, txt, ... etc.)
+    """
     
+    # get the directory of this script:
+    base_directory = os.path.dirname(__file__)
+    
+    # format the name of the path to be created:
+    directory_path = r'{}\\{}'.format(base_directory, file_type)
+    
+    # create directory if it does NOT exist:
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    else:
+        print("directory already exists ...")
+        
+
+def format_file_name(desired_job, file_type):
     """format file name without file extension (for example: ".txt", ".csv", ... etc.)
 
+    Args:
+        desired_job (String): the job you searched for
+        file_type (String): desired file type (for example: csv, txt, ... etc.)
+
     Returns:
-        String: constructed file name
+        String: constructed file path
     """
+    
     # format file name using current date and time:
     date_now = str(datetime.now()).split(":")
     date_now = "-".join(date_now)
@@ -15,7 +40,13 @@ def format_file_name(desired_job):
     file_name = str("{} {}".format(date_now, desired_job))
     print("Saving into file: {}".format(file_name))
     
-    return file_name
+    # get the directory of this script:
+    base_directory = os.path.dirname(__file__)
+    
+    # format the name of the path to be created:
+    file_path = r'{}\\{}\\{}'.format(base_directory, file_type, file_name)
+    
+    return file_path
 
 
 def count_found_jobs(data_found):
@@ -32,10 +63,21 @@ def count_found_jobs(data_found):
 
 
 def save_into_csv(desired_job, data_found):
-    # construct file name:
-    file_name = format_file_name(desired_job) + ".csv"
+    """save given data into .csv file
 
-    with open(file_name, 'w', encoding="utf-8") as csv_file:
+    Args:
+        desired_job (String): the job you searched for
+        data_found (Dict): constructed dict contains found data
+    """
+    
+    # create a directory if it does NOT exist:
+    file_type = "csv"
+    create_directory(file_type)
+    
+    # construct file name:
+    file_path = format_file_name(desired_job, file_type) + ".csv"
+        
+    with open(file_path, 'w', encoding="utf-8") as csv_file:
         # create csv writer object:
         csv_writer = csv.writer(csv_file)
         
