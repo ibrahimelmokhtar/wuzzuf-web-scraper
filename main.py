0cbd32de2d5e569ba860fa06ab3bfe13a5b00087
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2021-12-16 00:46:21 +0200

    Add try/except into main() function

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from checkUserInput import *
 from saveIntoFile import *
 from wuzzufAPI import *
 
-# get disired hob title from the user:
-desired_job = input("Enter job title: ")
-check_jobTitle(desired_job)
+def main():
+    # get disired hob title from the user:
+    desired_job = input("Enter job title: ")
+    check_jobTitle(desired_job)
 
-# extract specific data from wuzzuf.net:
-data_found = wuzzuf_api(desired_job)
+    # extract specific data from wuzzuf.net:
+    data_found = wuzzuf_api(desired_job)
 
-# save found data into .csv file:
-save_into_csv(desired_job, data_found)
+    # save found data into .csv file:
+    save_into_csv(desired_job, data_found)
 
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("Exception in main() function ...")
